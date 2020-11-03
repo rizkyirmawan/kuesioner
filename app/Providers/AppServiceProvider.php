cd3ec087b9fe64f625187fa9da83b350bf92ba55
Rizky Irmawan <rzkyirmwn@gmail.com>
2020-11-04 06:57:21 +0700

    Fix https scheme on boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('https');
+        if(config('app.env') !== 'local')
+        {
+            URL::forceScheme('https');
+        }
     }
 }
